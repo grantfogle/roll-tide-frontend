5b8df4fbee4aee69c3dde962cfc977f914436a60
S-Romberg <spencer.romberg@gmail.com>
2019-02-16 15:03:21 -0700

    added donate component

--- roll-tide/src/app/app.module.ts
+++ roll-tide/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { VideoComponent } from './video/video.component';
 import { MissionComponent } from './mission/mission.component';
+import { DonateComponent } from './donate/donate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     VideoComponent,
-    MissionComponent
+    MissionComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,
